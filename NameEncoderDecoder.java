2894bde521d4120f0cb16ad654a83c3066e6d020
Igoroni <58298721+Igoroni@users.noreply.github.com>
2020-12-17 18:49:18 +0200

    Update NameEncoderDecoder.java

--- NameEncoderDecoder.java
+++ NameEncoderDecoder.java
@@ -1,51 +1,26 @@
 public class NameEncoderDecoder {
-    public boolean areNamesEqual(String name1, String name2) {
-        return name1.equals(name2);
+    public String encode(String name) {
+        String replacedName = name
+        .replace("e", "1")
+        .replace("u", "2")
+        .replace("i", "3")
+        .replace("o", "4")
+        .replace("a", "5");
+
+        return "NOTFORYOU" + replacedName + "YESNOTFORYOU";
     }
+    
+    public String decode(String name) {
+        int startIndex = 9;
+        int endIndex = name.length() - 12;
+        String shortenedName = name.substring(startIndex, endIndex);
 
-    public String makeFullName(String firstName, String lastName) {
-        return firstName.toLowerCase() + " " + lastName.toUpperCase();
-    }
-
-    public boolean isNameLucky(String name) {
-        String lowerCaseName = name.toLowerCase();
-        return lowerCaseName.contains("a") || lowerCaseName.contains("o");
-    }
-
-    public String getNameCode(String name) {
-        String code = name.charAt(0) + String.valueOf(name.charAt(name.length() - 1));
-        return code.toUpperCase();
-    }
-
-    public boolean isMoneyName(String name) {
-        char firstLetter = name.charAt(0);
-        char lastLetter = name.charAt(name.length() - 1);
-
-        boolean firstLetterOk = firstLetter >= '0' && firstLetter <= '9';
-        boolean lastLetterOk = lastLetter >= '0' && lastLetter <= '9';
-
-        return firstLetterOk && lastLetterOk;
-    }
-
-    public boolean isInvisibleName(String name) {
-        return name.strip().isEmpty();
-    }
-
-    public String makeNamePositive(String name) {
-        return name
-                .replace("no", "yes")
-                .replace("No", "yes")
-                .replace("nO", "yes")
-                .replace("NO", "yes");
-    }
-
-    public String makeNameClean(String name) {
-        return "CLEAN" + name.strip() + "CLEAN";
-    }
-  
-  public String makeHalfOfName(String name) {
-        int finalIndex = name.length() / 2;
-        return name.substring(0, finalIndex);
+        return shortenedName
+        .replace("1", "e")
+        .replace("2", "u")
+        .replace("3", "i")
+        .replace("4", "o")
+        .replace("5", "a");
     }
 }
     
