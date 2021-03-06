a8a401ee94d88090af4c607bfe7fef73c7bf2cf1
Igoroni <58298721+Igoroni@users.noreply.github.com>
2021-03-06 12:37:40 +0200

    Update NameEncoderDecoder.java

--- NameEncoderDecoder.java
+++ NameEncoderDecoder.java
@@ -1,26 +1,20 @@
-public class NameEncoderDecoder {
+class NameEncoderDecoder {
     public String encode(String name) {
-        String replacedName = name
-        .replace("e", "1")
-        .replace("u", "2")
-        .replace("i", "3")
-        .replace("o", "4")
-        .replace("a", "5");
-
-        return "NOTFORYOU" + replacedName + "YESNOTFORYOU";
+        return "NOTFORYOU"  + name.replace('e', '1')
+                .replace('u', '2')
+                .replace('i', '3')
+                .replace('o', '4')
+                .replace('a', '5') + "YESNOTFORYOU";
     }
-    
-    public String decode(String name) {
-        int startIndex = 9;
-        int endIndex = name.length() - 12;
-        String shortenedName = name.substring(startIndex, endIndex);
 
-        return shortenedName
-        .replace("1", "e")
-        .replace("2", "u")
-        .replace("3", "i")
-        .replace("4", "o")
-        .replace("5", "a");
+    public String decode(String  name) {
+        return name.substring(9, name.length() -12)
+                .replace('1', 'e')
+                .replace('2', 'u')
+                .replace('3', 'i')
+                .replace('4', 'o')
+                .replace('5', 'a');
     }
 }
+
     
